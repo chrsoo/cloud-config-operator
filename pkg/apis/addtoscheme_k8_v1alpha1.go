380f4a554c0a4509837e35eb0b83b423d323f95a
Christoffer Soop <chrsoo@users.noreply.github.com>
2018-12-27 18:46:39 +0100

    Added CloudConfig and CloudConfigServer CRD's

---
+++ pkg/apis/addtoscheme_k8_v1alpha1.go
@@ -0,0 +1,10 @@
+package apis
+
+import (
+	"github.com/chrsoo/cloud-config-operator/pkg/apis/k8/v1alpha1"
+)
+
+func init() {
+	// Register the types with the Scheme so the components can map objects to GroupVersionKinds and back
+	AddToSchemes = append(AddToSchemes, v1alpha1.SchemeBuilder.AddToScheme)
+}
